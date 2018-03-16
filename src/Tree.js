c71baef154b504f97c509e4d91bde41b076a8b6a
chinagj07 <chinagj07@163.com>
2018-03-16 16:16:29 +0800

    modify by RN latest version
    According to the latest version of RN, import  { PropTypes } from 'react'  is not the correct way to write。 It should be the following style of writing: import PropTypes from 'prop-types';

--- src/Tree.js
+++ src/Tree.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import {ScrollView, View, Text, TouchableOpacity, Dimensions } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import MyUtils from './MyUtils'
